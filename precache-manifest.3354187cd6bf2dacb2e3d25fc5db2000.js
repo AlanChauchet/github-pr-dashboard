375ada009cbdeb110ac76d3493bb87603aa05d38
Alan CHAUCHET <alan.chauchet@gmail.com>
2021-03-11 12:15:42 +0100

    Updates

---
+++ precache-manifest.3354187cd6bf2dacb2e3d25fc5db2000.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06157ef513228ba063d275750abbe3a0",
+    "url": "/AlanChauchet/github-pr-dashboard/index.html"
+  },
+  {
+    "revision": "d03fc0ca22287c266148",
+    "url": "/AlanChauchet/github-pr-dashboard/static/css/main.4d52003b.chunk.css"
+  },
+  {
+    "revision": "4e1ca0a9d261ae972042",
+    "url": "/AlanChauchet/github-pr-dashboard/static/js/2.b35839cd.chunk.js"
+  },
+  {
+    "revision": "d03fc0ca22287c266148",
+    "url": "/AlanChauchet/github-pr-dashboard/static/js/main.94ff7490.chunk.js"
+  },
+  {
+    "revision": "ab12229dac1577be9d4a",
+    "url": "/AlanChauchet/github-pr-dashboard/static/js/runtime~main.7e6a48ff.js"
+  }
+]);
